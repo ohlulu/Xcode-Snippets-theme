90a33f69ffc421a8be708ea6565f291c111cfc9d
ohlulu <z30262226@gmail.com>
2020-03-07 21:10:33 +0800

    [ Update ] : 2020/03/07

---
+++ Templates/File Templates/Custom/inherit VC.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,21 @@
+//___FILEHEADER___
+
+import UIKit
+
+class ___FILEBASENAME___: BaseViewController {
+
+    // MARK: - Life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+}
+
+// MARK: Setup UI methods
+
+fileprivate extension ___FILEBASENAME___ {
+    func setupUI() {
+        
+    }
+}
