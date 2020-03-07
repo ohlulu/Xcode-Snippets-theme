5accbcc7bf86ef6ee747bb434f4adf425d130c56
ohlulu <z30262226@gmail.com>
2020-03-07 21:19:06 +0800

    [ Update ] : 2020/03/07

---
+++ Templates/File Templates/File Templates/Custom/ViewModel.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,33 @@
+//___FILEHEADER___
+
+import Foundation
+
+final class ___FILEBASENAME___: BaseViewModel {
+
+    // Relay or Subject
+
+    // Property
+
+    // life cycle
+    override init() {
+        super.init()
+    }
+}
+
+// MARK: - Output
+
+extension ___FILEBASENAME___ {
+    
+}
+
+// MARK: - Input
+
+extension ___FILEBASENAME___ {
+    
+}
+
+// MARK: - Helper
+
+private extension ___FILEBASENAME___ {
+    
+}
