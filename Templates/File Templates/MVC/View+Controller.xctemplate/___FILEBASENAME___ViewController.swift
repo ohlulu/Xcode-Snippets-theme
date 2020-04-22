bc1b134de1b337465085a7e48bc1d0cb2b93d8b4
ohlulu <z30262226@gmail.com>
2020-04-22 13:58:41 +0800

    [ Add ] : MVC templates

---
+++ Templates/File Templates/MVC/View+Controller.xctemplate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,46 @@
+//___FILEHEADER___
+
+import UIKit
+
+final class ___FILEBASENAME___: BaseViewController {
+
+    // UI element
+
+    // property
+
+    // Life cycle
+    override init() {
+        super.init()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - User action
+
+private extension ___FILEBASENAME___ {
+
+}
+
+// MARK: - Getter
+
+private extension ___FILEBASENAME___ {
+
+}
+
+// MARK: - Setup UI methods
+
+private extension ___FILEBASENAME___ {
+
+    func setupUI() {
+
+        navigationItem.title = ""
+    }
+}
